712585777f672074e57759b42914e18845b0b9ce
Anmin <Aminschleiter@googlemail.com>
2023-11-11 01:34:37 +0100

    Adding Tabs and Option to Import JSON File for scanning

--- src/app/app.py
+++ src/app/app.py
@@ -12,15 +12,30 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Erhalten der Feature-Daten vom POST-Request
-    features = [float(x) for x in request.form.values()] #Konvertierung in Float  
-    final_features = np.array(features).reshape(1, -1) #Konvertierung in Array mit 1 Zeile und n Spalten
-    prediction = model.predict_proba(final_features) #Vorhersage der Wahrscheinlichkeit 
-    output = '{0:.{1}f}%'.format(prediction[0][1] * 100, 2)  #Konvertierung in Prozentsatz
+    data = request.get_json()
+    print("Recieved data:", data)
+    if not data:
+        return jsonify({'error': 'Keine Daten erhalten'}), 400
 
+    feature_names = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
+    features = [data.get(feature, 0) for feature in feature_names]  # Fallback auf 0, wenn kein Wert vorhanden ist
+
+    # Konvertieren Sie alle Werte zu Floats
+    try:
+        features = [float(x) for x in features]
+    except ValueError as e:
+        return jsonify({'error': f'Ungültiger Wert im Feature-Array: {e}'}), 400
+
+    final_features = np.array(features).reshape(1, -1)
+
+    # Führen Sie die Vorhersage durch
+    prediction = model.predict_proba(final_features)
+    output = '{0:.{1}f}%'.format(prediction[0][1] * 100, 2)
     response_text = 'Die Betrugswahrscheinlichkeit liegt bei {}'.format(output)
-
+    print(response_text)  # Fügen Sie dies hinzu, um die Ausgabe zu sehen
     return jsonify({'prediction_text': response_text})
+
+
 
 
 if __name__ == "__main__":
--- src/app/static/scripts.js
+++ src/app/static/scripts.js
@@ -1,31 +1,49 @@
 $(document).ready(function() {
-    // Funktion zur Ausführung der Betrugsvorhersage
-    function makePrediction() {
-        var formData = $('#predictionForm').serialize();
+    // Funktion zur Steuerung der Eingabemethoden
+    function toggleInputMethod(method) {
+        if (method === 'json') {
+            $('#manualInputFields').hide();
+            $('.tab, #DatenEinfuegen').show();
+            $('#jsonSubmitButton, #clearButton').show();
+        } else if (method === 'manual') {
+            $('#manualInputFields, #predictButton').show(); // Zeige manuelle Eingabefelder und Vorhersage-Button
+            $('.tab, .tabcontent').hide();
+            $('#jsonSubmitButton, #clearButton').show();
+        }
+    }
 
-        $.ajax({ // AJAX-Request wird verwendet um den eingebegebenen Text an den Server zu senden (in unserem Fall die Flask-App)
-            type: 'POST', // Verwende POST-Methode
-            url: '/predict', // URL, an die die Daten gesendet werden sollen
-            data: formData, 
+    // Event-Listener für die Änderung der Eingabemethode
+    $('input[name="inputMethod"]').on('change', function() {
+        toggleInputMethod($(this).val());
+    });
+
+    // Initial die Sichtbarkeit basierend auf der ausgewählten Methode setzen
+    toggleInputMethod($('input[name="inputMethod"]:checked').val());
+
+    // Funktion zur Ausführung der Betrugsvorhersage
+    function makePrediction(dataObject) {
+        console.log("Sending prediction request with data:", dataObject);
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify(dataObject),
             success: function(response) {
+                console.log(response);
                 $('#predictionResult').text(response.prediction_text);
-                
-                var percentageString = response.prediction_text.match(/(\d+(\.\d+)?%)/); // Suche nach einer Zahl mit Prozentzeichen
-                var percentage = percentageString ? parseFloat(percentageString[0]) : 0; // Konvertiere den Wert in eine Zahl
-            
-                // Prüfe, ob die Wahrscheinlichkeit größer als 33% ist
+
+                var percentageString = response.prediction_text.match(/(\d+(\.\d+)?%)/);
+                var percentage = percentageString ? parseFloat(percentageString[0]) : 0;
+
                 if (percentage > 30) {
-                    // Zeige die Warnung nur einmal an
-                    if($('#fraudAlert').length === 0) {
+                    if ($('#fraudAlert').length === 0) {
                         $('#predictionResult').after('<div id="fraudAlert" style="color: red;">Es könnte sich bei dieser Transaktion um eine betrügerische Handlung handeln, diese erfordert weitere Analysen Ihrerseits!</div>');
-                        resultPosition = $('#fraudAlert').offset().top; // Speichere die Position der Warnung
                     }
                 } else {
-                    // Entferne die Warnung, falls sie existiert und die Bedingung nicht mehr zutrifft
                     $('#fraudAlert').remove();
                 }
-                $('html, body').animate({ // Scrolle zur Ergebnis-Anzeige
-                    scrollTop: resultPosition - 20 // Etwas Platz über dem Ziel für bessere Sichtbarkeit
+                $('html, body').animate({
+                    scrollTop: $('#fraudAlert').offset().top - 20
                 }, 500);
             },
             error: function(error) {
@@ -35,37 +53,165 @@ $(document).ready(function() {
         });
     }
 
-    $('#predictButton').on('click', function() {
-        makePrediction();
+    function getFormDataAsObject() {
+        var formData = {};
+        $('#predictionForm').find('input').each(function() {
+            if (this.name && this.value) {
+                formData[this.name] = this.value;
+            }
+        });
+        return formData;
+    }
+    // Event-Handler für den manuellen Vorhersage-Button
+    $('#manualPredictButton').on('click', function() {
+        var dataObject = getFormDataAsObject();
+        makePrediction(dataObject);
     });
 
+    // Event-Handler für den Datei-Vorhersage-Button
+    $('#filePredictButton').on('click', function() {
+        var fileInput = document.getElementById('fileInput');
+        var file = fileInput.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var dataObject = JSON.parse(e.target.result);
+                makePrediction(dataObject);
+            };
+            reader.readAsText(file);
+        }
+    });
+
+    // Event-Handler für den Vorhersage-Button
+    $('#predictButton').on('click', function() {
+        var dataObject = getFormDataAsObject();
+        makePrediction(dataObject);
+    });
+
+    // Event-Handler für den manuellen Vorhersage-Button
+    $('#manualPredictButton').on('click', function() {
+        var dataObject = getFormDataAsObject();
+        makePrediction(dataObject);
+    });
+
+
+
+    // Event-Handler für den Löschen-Button
     $('#clearButton').on('click', function() {
-        // Clear all input fields in the form
-        $('#predictionForm').find('input[type=text], textarea').val('');
-        // Clear the prediction result
+        $('.container').find('input[type=text], textarea').val(''); // Löschen aller Textfelder und Textareas
         $('#predictionResult').empty();
-        // Remove the fraud alert message if it exists
         $('#fraudAlert').remove();
     });
 
-    // JavaScript um JSON zu parsen und in die Felder einzufügen
-    $('#jsonSubmitButton').on('click', function() {
-        var jsonInput = $('#jsonInput').val();
+    // Event-Handler für den Löschen-Button im "Daten Einfügen"-Tab
+    $('#jsonClearButton').on('click', function() {
+        $('#jsonInput').val(''); // Löschen des JSON-Input-Feldes
+        $('#predictionResult').empty();
+        $('#fraudAlert').remove();
+    });
+
+    // Event-Handler für den Löschen-Button im "Daten Einladen"-Tab
+    $('#fileClearButton').on('click', function() {
+        $('#predictionResult').empty(); // Löschen des Vorhersageergebnisses
+        $('#fraudAlert').remove(); // Entfernen des Betrugsalarms, falls vorhanden
+        $('#fileInput').val(''); // Löschen der ausgewählten Datei
+        $('#fileNameDisplay').text(''); // Löschen des angezeigten Dateinamens
+    });
+
+
+    // Funktion zum Zuweisen von JSON-Daten zu Formularfeldern
+    function assignJsonToFields(jsonInput) {
         try {
-            var data = JSON.parse(jsonInput); // Versuche den eingegebenen Text als JSON zu parsen
-            for (var key in data) { // Iteriere über alle Eigenschaften des Objekts 
-                if (data.hasOwnProperty(key)) { // Prüfe, ob das Objekt die Eigenschaft besitzt
-                    var input = $('input[name="' + key + '"]'); // Suche nach dem Input-Feld mit dem Namen der Eigenschaft
-                    if (input) { 
-                        input.val(data[key]); // Füge den Wert der Eigenschaft in das Input-Feld ein
+            var data = JSON.parse(jsonInput);
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    var input = $('input[name="' + key + '"]');
+                    if (input.length) {
+                        input.val(data[key]);
                     }
                 }
             }
         } catch (e) {
             alert('Ungültiges JSON!');
+        }
+    }
+
+    // Event-Handler für den JSON-Upload-Button
+    $('#jsonSubmitButton').on('click', function() {
+        var jsonInput = $('#jsonInput').val();
+        if (!jsonInput.trim()) {
+            alert('Bitte geben Sie JSON-Daten ein.');
             return;
         }
+    
+        try {
+            var dataObject = JSON.parse(jsonInput);
+            makePrediction(dataObject);
+        } catch (e) {
+            alert('Ungültiges JSON! Bitte überprüfen Sie die Eingabe.');
+        }
+    });
 
-    makePrediction();
+    // Funktion zum Öffnen von Tabs
+    function openTab(tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].classList.remove("active");
+        }
+        document.getElementById(tabName).style.display = "block";
+        // Entfernen der aktiven Klasse für alle Tabs und Hinzufügen zur aktuellen
+        $('.tablinks[data-target="' + tabName + '"]').addClass('active');
+    }
+    $('.tablinks').click(function() {
+        var target = $(this).data('target');
+        openTab(target);
+    });
+
+    // Setzt den initialen Tab
+    openTab('DatenEinfuegen');
+
+    document.getElementById('showResultButton').addEventListener('click', function() {
+        var fileInput = document.getElementById('fileInput');
+        var file = fileInput.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var dataObject = JSON.parse(e.target.result);
+                makePrediction(dataObject); // Verwenden Sie Ihre existierende Vorhersagefunktion
+            };
+            reader.readAsText(file);
+        }
+    });
+    
+
+    // Tab-Funktionalität
+    document.querySelectorAll('.tablinks').forEach(function(tab) {
+        tab.onclick = function(evt) {
+            var target = this.getAttribute('data-target');
+            if(target){
+                openTab(evt, target);
+            }
+        };
     });
 });
+
+function showFileName() {
+    var fileInput = document.getElementById('fileInput');
+    var fileNameDisplay = document.getElementById('fileNameDisplay');
+    var showResultButton = document.getElementById('showResultButton');
+
+    if (fileInput.files.length > 0) {
+        if (fileNameDisplay) { // Überprüfen, ob das Element existiert
+            fileNameDisplay.textContent = fileInput.files[0].name;
+        }
+        if (showResultButton) { // Überprüfen, ob das Element existiert
+            showResultButton.style.display = 'inline-block';
+        }
+    }
+}
+
